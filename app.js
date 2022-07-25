1e5a7ed089d28664603156372be9192cff1f4bef
Yes Mac <yesomachado19@gmail.com>
2022-07-25 00:48:14 -0500

    Initial DOM impression of the multiplication table and general visual adjustments applied

--- app.js
+++ app.js
@@ -7,18 +7,55 @@ function helloName() {
   //document.getElementById('name').value
   let whatsName = document.querySelector('#hello-name')
   const showName = document.createElement('h3')
-  showName.innerText = yourName
+  showName.innerText = 'Hi ' + yourName + '!'
   whatsName.append(showName)
   showName.className = 'title-hello'
   // console.log('hey')
 }
 
+//
 function mulTable() {
   let number = prompt('Enter the name to consult')
-  let result = 0
+  let operation = 0
+  let result = []
 
-  for (let i = 1; i < 12; i++){
-    console.log(result = i * number)
+  let dad = document.querySelector('.dad')  
+  const showItemTable = document.createElement('p')
+  const showTable = document.createElement('div')
+
+  for (let i = 1; i < 13; i++){
+    operation = i * number
+    result.push(operation)
+    // console.log(operation = i * number)
+    // console.log(operation)
+    // console.log(result)
+    // let t = `${number} x ${i}`
+    // showTable.innerText = result + t
   }
-  return result
+
+  for (let i = 0; i < result.length; i++) {
+    showItemTable.innerText = number + 'x' + i + '=' + result
+    console.log(`${number} x ${i + 1} = ${result[i]}`)
+  }
+
+  showTable.className = 'mulTable'
+  showTable.appendChild(showItemTable)
+  dad.appendChild(showTable)
+  // console.log(operation)
+  // console.log(result)
+  // return result
+}
+
+//card about
+const activeAbout = document.querySelector('#about-nav')
+activeAbout.addEventListener('click', showAbout)
+
+const showAboutSelect = document.querySelector('.about')
+
+function showAbout() {
+  const isShowAbout = showAboutSelect.classList.contains('disguise')
+
+  if (!isShowAbout) {
+    showAboutSelect.classList.add('disguise')
+  }
 }
